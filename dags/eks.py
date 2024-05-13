4bb6e1975fd006e5419490042429e24892a0061e
EC2 Default User <ec2-user@ip-10-0-0-195.us-east-2.compute.internal>
2024-05-13 08:44:11 +0000

    dbtcicdnew

--- dags/eks.py
+++ dags/eks.py
@@ -1,65 +1,57 @@
-import os
-import boto3
-import json
-from datetime import datetime
+
 from pathlib import Path
 
-from airflow.decorators import dag
+from airflow import DAG
+from pendulum import datetime
+from cosmos.config import ProjectConfig
+
+from cosmos import (
+    ProfileConfig,
+    ExecutionConfig,
+    ExecutionMode,
+    DbtTaskGroup,
+)
+from cosmos.profiles import RedshiftUserPasswordProfileMapping
 from airflow.operators.empty import EmptyOperator
 
-from cosmos.providers.dbt import DbtTaskGroup
+PROJECT_DIR = Path("dags/dbt/dbtcicdnew/")
 
 
-@dag(
-    schedule_interval="@daily",
-    start_date=datetime(2023, 7, 24),
+with DAG(
+    dag_id="dbtcicdnew",
+    start_date=datetime(2022, 11, 27),
+    doc_md=__doc__,
     catchup=False,
-)
-def basic_eks_cosmos_task_group() -> None:
-   
-    pre_dbt = EmptyOperator(task_id="pre_dbt")
+) as dag:
 
 
-    _project_dir= "/usr/local/airflow/dags/dbt/"
-
-    redshift_dbt_group = DbtTaskGroup(
-        # profile_args = {
-        #     "schema": "public",
-        # },
-        # profile_name_override = 'demo_redshift_tpch',
-        # target_name_override = 'dev',
-
-        dbt_root_path=_project_dir,
-        dbt_project_name="dbtcicd",
-        execution_mode="kubernetes",
-        conn_id="redshift-default",
+    run_models = DbtTaskGroup(
+        profile_config=ProfileConfig(
+            profile_name="dbtcicdnew",
+            target_name="dev",
+            profile_mapping=RedshiftUserPasswordProfileMapping(
+                conn_id="redshift_default",
+                profile_args={
+                    "schema": "public",
+                },
+            ),
+        ),
+        project_config=ProjectConfig(PROJECT_DIR),
+        execution_config=ExecutionConfig(
+            execution_mode=ExecutionMode.KUBERNETES,
+        ),
         operator_args={
             "do_xcom_push": False,
             "project_dir":"/app",
-            "image": "139260835254.dkr.ecr.us-east-2.amazonaws.com/dbtcicd:1.0",
+            "image": "139260835254.dkr.ecr.us-east-2.amazonaws.com/dbtcicdnew:1.0",
             "get_logs": True,
-            "is_delete_operator_pod": True,
-            "name": "mwaa-cosmos-pod-dbt",
+            "is_delete_operator_pod": False,
+            "namespace": "mwaa",
             "config_file": "/usr/local/airflow/dags/kubeconfig",
             "in_cluster": False,
-            "vars": '{"my_car": "val1"}',
-            "env_vars": {
-                "TARGT": "dev",
-                "HOST": 'lin-test.139260835254.us-east-2.redshift-serverless.amazonaws.com', 
-                "PORT": '5439',
-                "USER": 'admin',
-                "PASSWORD": '',
-                "DATABASE": 'dev',
-                "SCHEMA": 'public',
-            },
             "image_pull_policy": "Always",
         },
     )
-
-    post_dbt = EmptyOperator(task_id="post_dbt")
-
-    pre_dbt >> redshift_dbt_group >> post_dbt
-
-
-
-basic_eks_cosmos_task_group()
+    e1 = EmptyOperator(task_id="pre_dbt")
+    e2 = EmptyOperator(task_id="post_dbt")
+    e1 >> run_models >> e2
